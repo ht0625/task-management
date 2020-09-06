cea0782e86e7a138d7e5029ee2b8de4a77683ff5
hiroki tamura <hiirookii0625@gmail.com>
2020-09-06 23:46:42 +0900

    Task登録機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,22 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path,notice: "登録しました"
+    else
+      render :new
+    end
+  end
   def show
     @task = Task.find(params[:id])
+  end
+  private
+  def task_params
+    params.require(:task).permit(:name, :content)
   end
 end
