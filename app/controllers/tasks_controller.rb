31f6ab644a287cbcb0a5c9308bf52757fb8ca859
hiroki tamura <hiirookii0625@gmail.com>
2020-09-07 10:12:47 +0900

    Task編集機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,9 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
+
   def new
     @task = Task.new
   end
@@ -13,11 +15,27 @@ class TasksController < ApplicationController
       render :new
     end
   end
+
   def show
     @task = Task.find(params[:id])
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "編集しました。"
+    else
+      render :edit
+    end
+  end
+
   private
+
   def task_params
     params.require(:task).permit(:name, :content)
   end
+
 end
