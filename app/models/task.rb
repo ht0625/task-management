0461e1087954f34bda953f2cf946ec7650feabbf
hiroki tamura <hiirookii0625@gmail.com>
2020-09-12 13:20:58 +0900

    tasksテーブルにpriorityカラム追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
   validates :content, presence: true
 
   scope :name_search, -> (name){ where('name LIKE ?', "%#{name}%") }
-  scope :status_search, -> (status){ where(status: "#{status}")} 
+  scope :status_search, -> (status){ where(status: "#{status}")}
 
+  enum priority:{  low: 0, medium: 1, high: 2 }
 end
