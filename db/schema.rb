2a69794f833040ae55d5f6c7272ebb797c723d84
hiroki tamura <hiirookii0625@gmail.com>
2020-09-13 15:38:24 +0900

    usersテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_024556) do
+ActiveRecord::Schema.define(version: 2020_09_13_062745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_09_12_024556) do
     t.string "status", default: "未着手", null: false
     t.integer "priority", default: 0, null: false
     t.index ["name", "status"], name: "index_tasks_on_name_and_status"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
