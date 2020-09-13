021d021b245d2d20afb3a6e0431855b045eb5e74
hiroki tamura <hiirookii0625@gmail.com>
2020-09-13 19:44:22 +0900

    ログイン機能実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      render :new, notice: 'ログインに失敗しました'
+    end
+  end
+end
