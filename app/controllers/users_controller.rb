fbc01227fcd0de38fd72f584a8b2c1efa875e955
hiroki tamura <hiirookii0625@gmail.com>
2020-09-13 17:19:29 +0900

    user詳細画面実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功した場合の処理
+      redirect_to user_path(@user.id)
     else
       render :new
     end
+
+    def show
+      @user = User.find(params[:id])
+    end
+
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
 end
