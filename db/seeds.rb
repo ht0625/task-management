0d25698d8caf27c238ae7845a0d238aac51005ea
hiroki tamura <hiirookii0625@gmail.com>
2020-09-17 17:59:01 +0900

    シードデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,50 @@
-9.times do |n|
-  name = "name#{n}"
-  email = "task#{n}@example.com"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               )
-end
+# 9.times do |n|
+#   name = "name#{n}"
+#   email = "task#{n}@example.com"
+#   password = "password"
+#   User.create!(name: name,
+#                email: email,
+#                password: password,
+#                password_confirmation: password,
+#                )
+# end
 
-User.create!(name: "admin",
-             email: "admin22@example.com",
-             password: "password",
-             password_confirmation: "password",
+# User.create!(name: "admin",
+#              email: "admin22@example.com",
+#              password: "password",
+#              password_confirmation: "password",
+#              admin: true
+#              )
+
+# (1..10).each do |n|
+#   label_type = "label#{n}"
+#   Label.create!(label_type: label_type,
+#                )
+# end
+#
+# (1..10).each do |n|
+#   Task.create!(
+#     name: "タスク#{n}",
+#     content: "タスク内容#{n}",
+#     user_id:  User.first.id
+#   )
+# end
+name = Faker::Games::Pokemon.name
+email = Faker::Internet.email
+password = "password"
+User.create!(name: name,
+             email: email,
+             password: password,
+             password_confirmation: password,
              admin: true
              )
-
-(1..10).each do |n|
-  label_type = "label#{n}"
-  Label.create!(label_type: label_type,
-               )
-end
-
-(1..10).each do |n|
-  Task.create!(
-    name: "タスク#{n}",
-    content: "タスク内容#{n}",
-    user_id:  User.first.id
-  )
+9.times do |n|
+ name = Faker::Games::Pokemon.name
+ email = Faker::Internet.email
+ password = "password"
+ User.create!(name: name,
+              email: email,
+              password: password,
+              password_confirmation: password,
+              )
 end
